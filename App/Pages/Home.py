c8ec55af8bc76ef4bb925b80497b6010cc059af2
Brandonxay <48526818+Brandonxay@users.noreply.github.com>
2022-04-26 20:20:33 -0400

    Update Home.py

--- App/Pages/Home.py
+++ App/Pages/Home.py
@@ -10,10 +10,15 @@ class TheHomePage:
         # TODO: define the streamlit view here
         st.header("Predicting Heart Desease")
         
-        st.header("We are looking for correlations in the data that may lead to heart diease");
+        st.header("We are looking for correlations in the data that may lead to heart diease")
         
         BMI = st.number_input('Insert your BMI')
         
+        option = st.selectbox(
+     'Have you ever been told you had a stroke',
+     ('Yes', 'No'))
+
+
         
         
         
